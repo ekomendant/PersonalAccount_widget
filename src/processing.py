b85762f7f78379ab366d8e204691e86efe814950
Evgenia Komendant <eekomendant@gmail.com>
2025-04-16 16:21:03 +0300

    Добавлена функция sort_by_date в processing.py

--- src/processing.py
+++ src/processing.py
@@ -3,8 +3,8 @@ def filter_by_state(states_list: list[dict], state: str = "EXECUTED") -> list[di
     Функция принимает список словарей со всеми статусами и отфильтровывает по статусу,
     переданному вторым аргументом
     :param states_list: неотфильтрованный список словарей со всеми статусами
-    :param state: отфильтрованный список словарей с выбранным статусом
-    :return:
+    :param state: статус для фильтрации, по умолчанию "EXECUTED"
+    :return: отфильтрованный список словарей с выбранным статусом
     """
 
     filtered_states = list()
@@ -13,3 +13,16 @@ def filter_by_state(states_list: list[dict], state: str = "EXECUTED") -> list[di
             filtered_states.append(element)
 
     return filtered_states
+
+
+def sort_by_date(states_list: list[dict], reverse_value: bool = True) -> list[dict]:
+    """
+    Функция принимает список словарей со статусами/датами и сортирует его в порядке,
+    переданном вторым аргументом
+    :param states_list: неотсортированный список словарей
+    :param reverse_value: булевое значение для реверса, по умолчанию True
+    :return: отсортированный по дате список словарей
+    """
+
+    sorted_list = sorted(states_list, key=lambda x: x["date"], reverse=reverse_value)
+    return sorted_list
